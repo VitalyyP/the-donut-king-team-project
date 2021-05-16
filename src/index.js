303bea3cb36fd648781bd85f36abff936baef6dc
Vitalyy Pikozh <wisart@gmail.com>
2021-05-16 19:51:48 +0300

    copy

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
-import './sass/main.scss';
+import './sass/main.scss'
+
+$(function() {
+
+    $('.reviews-list').slick({
+        // dots: true,
+        arrows: false,
+        infinite: false,
+        centerMode: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        variableWidth: true,
+        responsive: [
+            {
+              breakpoint: 767,
+              settings: {
+                slidesToShow: 1,
+              }
+            }
+        ],
+        adaptiveHeight: true
+    })
+
+})
+
