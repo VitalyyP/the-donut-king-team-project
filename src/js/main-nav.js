10ac347f554a213fc8dad8884071e9cbb90db473
Vitalyy Pikozh <wisart@gmail.com>
2021-05-18 23:24:03 +0300

    fix main-nav.js

---
+++ src/js/main-nav.js
@@ -0,0 +1,14 @@
+// $(function () {
+//   $('.menuToggle').on('click', function () {
+//     $('.main-nav').slideToggle(500, function () {
+//       if ($(this).css('display') === 'none') {
+//         $(this).removeAttr('style');
+//       }
+//     });
+//   });
+//   $('.main-nav__link').on('click', function () {
+//     $('.main-nav').hide();
+//     // $('.mobile-menu_close').hide();
+//   });
+// });
+
--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,10 +2,17 @@
 ;(() => {
     const menuBtnRef = document.querySelector("[data-menu-button]")
     const mobileMenuRef = document.querySelector("[data-menu]")
+    const mobileNavList = document.querySelector('[data-menu-list]');
     menuBtnRef.addEventListener("click", () => {
       const expanded = menuBtnRef.getAttribute("aria-expanded") === "true" || false
       menuBtnRef.classList.toggle("is-open")
       menuBtnRef.setAttribute("aria-expanded", !expanded)
       mobileMenuRef.classList.toggle("is-open")
+    })
+    mobileNavList.addEventListener("click", () => {
+      const expanded = mobileNavList.getAttribute("aria-expanded") === "true" || false
+      mobileMenuRef.classList.toggle('is-open');
+      mobileNavList.setAttribute("aria-expanded", !expanded)
+      menuBtnRef.classList.toggle('is-open');
     })
   })()
